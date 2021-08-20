41b5ee81861dcfcedb6d2209b90f5ee9b81a5c2d
hahuaz <hahuaz@gmail.com>
2021-08-20 22:45:36 +0300

    add menu to about nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,46 @@
   <v-app>
     <v-app-bar app flat hide-on-scroll height="180">
       <div class="tw-grid tw-grid-cols-3 tw-w-full">
-        <div class="navs tw-flex tw-gap-6 tw-items-center">
+        <div class="nav tw-flex tw-gap-6 tw-items-center">
           <nuxt-link to="/">Home</nuxt-link>
-          <nuxt-link to="/about">About</nuxt-link>
+          <v-menu open-on-hover bottom offset-y>
+            <template #activator="{ on, attrs }">
+              <a
+                v-bind="attrs"
+                href="javascript:void(0)"
+                :class="{
+                  'nuxt-link-exact-active':
+                    $route.path === '/uppercase' ||
+                    $route.path === '/founders-story' ||
+                    $route.path === '/team',
+                }"
+                v-on="on"
+              >
+                About
+              </a>
+            </template>
+
+            <v-list color="blue-dark">
+              <v-list-item>
+                <v-list-item-title
+                  ><nuxt-link to="/uppercase"
+                    >Uppercase</nuxt-link
+                  ></v-list-item-title
+                >
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title
+                  ><nuxt-link to="/founders-story"
+                    >Founder's Story</nuxt-link
+                  ></v-list-item-title
+                > </v-list-item
+              ><v-list-item>
+                <v-list-item-title
+                  ><nuxt-link to="/team">Team</nuxt-link></v-list-item-title
+                >
+              </v-list-item>
+            </v-list>
+          </v-menu>
           <nuxt-link to="/testimonials">Testimonials</nuxt-link>
           <nuxt-link to="/case-studies">Case Studies</nuxt-link>
         </div>
@@ -79,16 +116,19 @@ export default {
 .v-app-bar.v-app-bar--is-scrolled {
   @apply tw-bg-blue-dark !important;
 }
+
+.nav a,
+.v-menu__content a {
+  @apply tw-text-gray-50 tw-font-medium tw-text-xl !important;
+}
+
+.nav a.nuxt-link-exact-active {
+  border-bottom: 1px solid white;
+}
 </style>
 <style lang="scss" scoped>
 ::v-deep .v-toolbar__content {
   padding: 30px 50px;
-}
-
-.navs {
-  a {
-    @apply tw-text-gray-50 tw-font-medium tw-text-xl;
-  }
 }
 
 footer {
